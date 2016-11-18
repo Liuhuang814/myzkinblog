ddf7a63cd28221f807a682eccbf3927c2a68026b
Liuhuang814 <1419468810@qq.com>
2016-11-18 14:17:37 +0800

    11-18日

--- app.js
+++ app.js
@@ -2,33 +2,66 @@ var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
+var session=require('express-session');
+var MongoStore=require('connect-mongo')(session);
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
+//加载路由文件  routes文件夹用来存放路由文件
 var index = require('./routes/index');
 var users = require('./routes/users');
+var articles = require('./routes/articles');
+var config=require('./dbconfig');
 
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+//app.set('view engine', 'ejs');
+
+app.set('view engine','html');
+app.engine('html',require('ejs').__express);
+
+//解析session
+app.use(cookieParser());
+app.use(session({
+  secret:'zkingblog',//加密cookie,防止cookie被篡改
+  resave:true,//表示每次请求处理完毕后都更新session数据
+  cookie:{maxAge:1000*60*30},//设置session有效时间为30分钟
+  saveUninitialized:true,//保存新创建但是会初始化的session
+  //把session的信息保存到数据库中
+  store: new MongoStore({url: config.dburl})
+}));
+
+//由于需要给每个页面在渲染时传递session中保存的user对象,所以可以添加一个中间件,专门处理
+//session的问题
+app.use(function(req,res,next){
+  res.locals.user=req.session.user;
+  next();
+});
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
+//设置表单格式  需要两种格式  json 和 urlencoded
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+//加载解析cookie
 app.use(cookieParser());
+
+//设置静态目录
 app.use(express.static(path.join(__dirname, 'public')));
 
+//设置路由处理模块  所有访问'/'网站根目录的请求都由index路由模块处理
 app.use('/', index);
-app.use('/users', users);
+//所有访问'/user'网站目录的请求都由users路由模块处理
+app.use('/user', users);
 
+app.use('/article', articles);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
+  //转到下一个中间件,做错误页面的渲染
   next(err);
 });
 
@@ -42,5 +75,5 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
-
+//将APP暴露给外界
 module.exports = app;
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,86 @@
 var express = require('express');
 var router = express.Router();
+var models=require('../db/model.js');
+var utils=require('../utils.js');
+var auth=require('../middleware/autoauth');
 
-/* GET users listing. */
+/* GET user listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
+});
+
+/**
+ * 用户注册
+ */
+router.get('/reg',auth.checkNotLogin, function (req, res) {
+  res.render('user/reg', {title: '用户注册'});
+});
+
+/**
+ * 当填写用户注册信息提交时的处理
+ * 路径与上面访问的主责页面路径是一致的,只是动作是POST
+ * 这种设计即是RESTful设计原则
+ *
+ */
+router.post('/reg', function (req, res,next) {
+  //获取表单数据
+  var user=req.body;
+  if(user.password==user.repassword){
+    models.User.findOne({username:user.username}, function (err, doc) {
+      if(doc){
+        //如果有值,表示用户名已存在
+
+      }else{
+        //没有值才能够注册
+        models.User.create(
+            //脱库攻击
+            {username:user.username, password:utils.md5(user.password),
+          email:user.email}, function (err, doc) {
+          if(err){
+
+          }else{
+            //注册成功,重定向到登录页面
+            res.redirect('/user/login');
+          }
+        })
+      }
+    })
+
+  }else{
+    //两次密码不一致
+  }
+});
+
+/**
+ * 显示用户登录表单
+ */
+router.get('/login',auth.checkNotLogin, function (req, res) {
+  res.render('user/login', {title: '登录'});
+});
+
+/**
+ * 当填写用户登录信息提交时的处理
+ */
+router.post('/login', function (req, res) {
+  var user=req.body;
+
+  models.User.findOne({username:user.username,
+    password:utils.md5(user.password)}, function (err, doc) {
+    if(doc){
+      //如果doc存在,那么就是登录成功
+      //登陆成功后,将用户的信息放入session保存
+      req.session.user=doc;
+      res.redirect('/')
+    }else{
+      //如果doc不存在,那么就是登录失败
+      res.redirect('/user/login')
+    }
+  });
+});
+
+router.get('/logout',auth.checkLogin, function (req, res) {
+  req.session.user=null;
+  res.redirect('/')
 });
 
 module.exports = router;
